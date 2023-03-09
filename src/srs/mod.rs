cbff382c7c328fbce61dd8f6c5a2fbf201525d55
zoop-potato <105884476+zoop-potato@users.noreply.github.com>
2023-03-09 15:56:36 +0000

    Fill in the Implementation for Scope

--- src/srs/mod.rs
+++ src/srs/mod.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 pub mod utils;
+pub mod scope;
 use crate::srs::utils::*;
 
 #[derive(Serialize, Deserialize)]
--- src/srs/scope.rs
+++ src/srs/scope.rs
@@ -1,9 +1,30 @@
 /// Fast and simple way to have both start and end indexs into a string AND all the functinality of &str
+#[derive(Debug, Clone, Copy)]
 pub struct Scope<'a> {
     string: &'a str,
     start_index: usize,
 }
 
-impl Scope<'a> {
-    
+impl<'a> Scope<'a> {
+    pub fn new(start_index: usize, string: &'a str) -> Self {
+        Scope { string: string, start_index: start_index }
+    }
+
+    pub fn start_index(&self) -> usize {
+        self.start_index
+    }
+
+    pub fn end_index(&self) -> usize {
+        self.start_index + self.string.len()
+    }
+
+    pub fn range(&self) -> std::ops::RangeInclusive<usize> {
+        self.start_index..=self.end_index()
+    }
+}
+
+impl <'a> From<Scope<'a>> for &'a str {
+    fn from(val: Scope<'a>) -> Self {
+        val.string
+    }
 }
